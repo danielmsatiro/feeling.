5a65d6cea47e5491f0fae757fb049fa869b8153c
Pedro Basilio <pedronobasi@gmail.com>
2022-01-26 16:33:43 -0300

    feat: home firstPart

---
+++ src/pages/Home/firstPart.jsx
@@ -0,0 +1,11 @@
+import {Flex, Grid, Heading, Text} from "@chakra-ui/react"
+
+export const HomeFirstPart = () => {
+    return (
+        <Grid>
+            <Flex bg="yellow.50">
+                <Heading color="orange.500" as="h1"> feeling. </Heading>
+            </Flex>
+        </Grid>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,11 @@
-import { Text } from "@chakra-ui/react";
+import { Grid, Text } from "@chakra-ui/react";
+import { HomeFirstPart } from "./firstPart";
 
 export const Home = () => {
-  return <Text color="black">Home</Text>;
+  return (
+    <Grid>
+      <Text color="black">Home</Text>
+      <HomeFirstPart />
+    </Grid>
+  )
 };
