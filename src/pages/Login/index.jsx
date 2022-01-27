fdf6b8a0498a321f88964d10dc535bada4911e7b
HardBoris <bgaibor03@gmail.com>
2022-01-26 23:10:05 -0400

    feat: login page ready without procedures

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,49 +1,9 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  Heading,
-  Input,
-  Text,
-} from "@chakra-ui/react";
-import { ControlledInput } from "../../components/Input/ControlledInput";
-import { PasswordInput } from "../../components/Input/PasswordInput";
+import { LoginForm } from "./LoginForm";
 
 export const Login = () => {
   return (
-    <Flex
-      as="form"
-      w={["300px"]}
-      h={["500px"]}
-      margin="auto"
-      flexDirection={["column"]}
-    >
-      <Flex h={["150px"]} justifyContent={["center"]} align={["center"]}>
-        <Text color="orange.500" fontSize={["6xl"]} textAlign="center">
-          feeling.
-        </Text>
-      </Flex>
-      <Flex h={["80px"]} justifyContent={["flex-start"]} align={["center"]}>
-        <Text fontSize={["3xl"]}>login.</Text>
-      </Flex>
-
-      <ControlledInput placeholder="Email" name="email" />
-
-      <ControlledInput placeholder="Senha" name="password" />
-      <Flex h={["80px"]} alignItems={["center"]}>
-        <Button
-          w={["100%"]}
-          h={["50px"]}
-          pr="4.5rem"
-          bg="orange.500"
-          color="yellow.50"
-          borderRadius="30px"
-          padding="0"
-        >
-          entrar
-        </Button>
-      </Flex>
-    </Flex>
+    <>
+      <LoginForm />
+    </>
   );
 };
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,12 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 export const SignUp = () => {
-  return <Text>SignUp</Text>;
+  const history = useHistory();
+  return (
+    <Box>
+      <Text>SignUp</Text>
+      <Button onClick={() => history.push("/login")}>Login</Button>
+    </Box>
+  );
 };
