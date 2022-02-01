55a084c76244ee9d5df156ef0c1d14ee4f017fcb
Pedro Basilio <pedronobasi@gmail.com>
2022-02-01 11:17:06 -0300

    feat: saudaçao ao usuario

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,16 @@
 import { Header } from "../../components/Header";
 import { DashboardContent } from "./DashboardContent";
+import { useAuth } from "../../provider/AuthContext"
 
 export const Dashboard = () => {
+
+  const {user} = useAuth()
+  console.log(user.name)
+
   return (
     <>
       <Header />
-      <DashboardContent />
+      <DashboardContent name={user.name}/>
     </>
   );
 };
