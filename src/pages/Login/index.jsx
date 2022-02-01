bf94085a3feb3bd7c6890d9c77451c7f47a1a27e
Guilherme Couto <gcoutos95@gmail.com>
2022-02-01 15:08:37 -0300

    fix: Fixing the yup bug + Refactoring the signup and login styling

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,29 +1,11 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { LoginForm } from "./LoginForm";
 import { LoginImage } from "./LoginImage";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useAuth } from "../../provider/AuthContext";
+
 import { useHistory } from "react-router-dom";
 
-const signInSchema = yup.object().shape({
-  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
-  password: yup.string().required("Senha obrigatória"),
-});
 export const Login = () => {
-  const history = useHistory()
-  const { signIn } = useAuth();
-  const {
-    formState: { errors },
-    register,
-    handleSubmit,
-  } = useForm({ resolver: yupResolver(signInSchema) });
-
-  const Sender = (data) => {
-    signIn(data);
-  };
-
+  const history = useHistory();
   return (
     <Flex
       maxW="1500px"
@@ -33,13 +15,13 @@ export const Login = () => {
       flexDirection={["column"]}
       alignItems="center"
     >
-      <Flex 
+      <Flex
         onClick={() => history.push("/")}
         cursor="pointer"
-        h={["150px"]} 
-        justifyContent={["center"]} 
-        alignItems={["flex-end"]} 
-        pb="28px" 
+        h={["150px"]}
+        justifyContent={["center"]}
+        alignItems={["flex-end"]}
+        pb="28px"
         pr="15px"
       >
         <Text
@@ -50,20 +32,12 @@ export const Login = () => {
           feeling.
         </Text>
       </Flex>
-      <Flex 
-        mt="20px" 
-        justifyContent={["space-between"]} 
-        align={["center"]}>
+      <Flex mt="20px" justifyContent={["space-between"]} align={["center"]}>
         <Flex>
           <LoginImage />
         </Flex>
-        <Flex w={["300px", "300px", "350px", "400px"]}>
-          <LoginForm
-            sender={handleSubmit(Sender)}
-            register={register}
-            error={errors}
-          />
-        </Flex>
+
+        <LoginForm />
       </Flex>
     </Flex>
   );
