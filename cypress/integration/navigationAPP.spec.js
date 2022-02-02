743397787e2d9795263b55a00d4a0c5cd8393a84
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-02-02 11:58:18 -0300

    feat: add cypress in first page

--- cypress/integration/navigationAPP.spec.js
+++ cypress/integration/navigationAPP.spec.js
@@ -1,8 +1,24 @@
 context("NavigationAPP", () => {
-  it("Enters the landing page and tries to go to the bottom the page", () => {
-    cy.visit("https://localhost:3000");
-    cy.viewport(1440, 900);
-    cy.scrollTo("bottom", { duration: 10000 });
-    cy.contains("voltar ao topo");
+  it("Scrolling first page to the bottom and returns to top", () => {
+    cy.visit("http://localhost:3000");
+    cy.viewport("macbook-15");
+    cy.scrollTo(0, "0%", { duration: 2000 });
+    cy.scrollTo(0, "20%", { duration: 3000 });
+    cy.scrollTo(0, "20%", { duration: 1000 });
+    cy.scrollTo(0, "40%", { duration: 3000 });
+    cy.scrollTo(0, "40%", { duration: 1000 });
+    cy.scrollTo(0, "60%", { duration: 3000 });
+    cy.scrollTo(0, "60%", { duration: 1000 });
+    cy.scrollTo(0, "80%", { duration: 3000 });
+    cy.scrollTo(0, "80%", { duration: 1000 });
+    cy.scrollTo(0, "100%", { duration: 3000 });
+    cy.scrollTo(0, "100%", { duration: 2000 });
+    cy.contains("voltar ao topo").click();
+    cy.viewport("ipad-2");
+    cy.scrollTo("bottom", { duration: 5000 });
+    cy.contains("voltar ao topo").click();
+    cy.viewport("iphone-5");
+    cy.scrollTo("bottom", { duration: 5000 });
+    cy.contains("voltar ao topo").click();
   });
 });
