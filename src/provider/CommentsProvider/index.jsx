b81f5a79dd5444c5406fd8fefe0b5eda71bfe82c
Guilherme Couto <gcoutos95@gmail.com>
2022-02-02 09:58:51 -0300

    feat: fixing the exhaustive-deps bug

--- src/provider/CommentsProvider/index.jsx
+++ src/provider/CommentsProvider/index.jsx
@@ -1,64 +1,76 @@
 import {
-    createContext,
-    useCallback,
-    useContext,
-    useEffect,
-    useState,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
 } from "react";
 import { api } from "../../services/api";
 import { useAuth } from "../AuthContext";
 
-const CommentsContext = createContext({})
+const CommentsContext = createContext({});
 
 const useComments = () => {
-    const context = useContext(CommentsContext);
+  const context = useContext(CommentsContext);
 
-    if (!context) {
-        throw new Error("usePhrases must be used within an CommentsProvider");
-    }
-    return context
-}
+  if (!context) {
+    throw new Error("usePhrases must be used within an CommentsProvider");
+  }
+  return context;
+};
 
-const CommentsProvider = ({children}) => {
+const CommentsProvider = ({ children }) => {
+  const { user, accessToken } = useAuth();
+  const [myComments, setMyComments] = useState([]);
 
-    const {user, accessToken} = useAuth()
-    const [myComments, setMyComments] = useState([])
+  const getMyComments = () => {
+    api
+      .get(`comments?userId=${user.id}&_expand=phrase`)
+      .then((res) => setMyComments(res.data));
+  };
 
-    const getMyComments = () => {
-        api.get(`comments?userId=${user.id}&_expand=phrase`).then((res) => setMyComments(res.data))
-    } 
+  useEffect(() => {
+    getMyComments();
 
-    useEffect(() => {
-        getMyComments()
-    }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const deleteMyComments = (commentId) => {
-        api.delete(`comments/${commentId}`, {
-            headers: {
-                authorization: `Bearer ${accessToken}`
-            }
-        }).then((_) => {getMyComments()})
-    }    
+  const deleteMyComments = (commentId) => {
+    api
+      .delete(`comments/${commentId}`, {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((_) => {
+        getMyComments();
+      });
+  };
 
-    const UpdateComment = (commentId, value, onClose) => {
-        api.patch(`comments/${commentId}`, value, {
-            headers: {
-                authorization: `Bearer ${accessToken}`
-            }
-        }).then((_) => {getMyComments()}).then(onClose)
-    }
+  const UpdateComment = (commentId, value, onClose) => {
+    api
+      .patch(`comments/${commentId}`, value, {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((_) => {
+        getMyComments();
+      })
+      .then(onClose);
+  };
 
-    return (
-        <CommentsContext.Provider
-            value={{
-                myComments, 
-                deleteMyComments,
-                UpdateComment
-            }}
-        >
-            {children}
-        </CommentsContext.Provider>
-    )
-}
+  return (
+    <CommentsContext.Provider
+      value={{
+        myComments,
+        deleteMyComments,
+        UpdateComment,
+      }}
+    >
+      {children}
+    </CommentsContext.Provider>
+  );
+};
 
-export {useComments, CommentsProvider}
+export { useComments, CommentsProvider };
